976da15c11557f19076819c33d6d37919d012f09
amanita-web <tobiasknauer2004@gmail.com>
2026-02-06 19:37:28 +0100

    Refactor game logic and improve guess submission

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ function reset(){
         tBlock.style.backgroundColor = "gray";
         tText = document.getElementById(`letter5${i}`);
         tText.textContent = "---";
+    }
 }
-
 
 function generateSolution(solution){
     return solutions[Math.floor(Math.random() * solutions.length)];
@@ -86,6 +86,7 @@ function submitGuess(){
     
 
 }
+
 
 
 
